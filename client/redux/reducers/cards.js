f3a159dc115e24502d6d3904322251b8695d641a
Stuart Walsh <stu@ipng.org.uk>
2020-06-22 09:29:44 +0100

    PendingGame and related component refactor

--- client/redux/reducers/cards.js
+++ client/redux/reducers/cards.js
@@ -91,7 +91,7 @@ export default function (state = { decks: [], cards: [] }, action) {
             return Object.assign({}, state, {
                 player2CardBack: action.url
             });
-        case 'RECEIVE_DECKS':
+        case Decks.DecksReceived:
             processDecks(action.response.decks, state);
             newState = Object.assign({}, state, {
                 singleDeck: false,
@@ -117,7 +117,7 @@ export default function (state = { decks: [], cards: [] }, action) {
                 deckSaved: false,
                 deckDeleted: false
             });
-        case 'REQUEST_DECKS':
+        case Decks.RequestDecks:
             newState = Object.assign({}, state, {
                 deckSaved: false,
                 deckDeleted: false
